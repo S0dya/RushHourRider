b28cc9b61644689d0990637d34614a7b5078b9b9
Cowpoke2 <pepsipsycho21@gmail.com>
2023-10-13 03:19:00 +0400

    input

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
+public class Player : SingletonMonobehaviour<Player>
 {
-    // Start is called before the first frame update
+    Inputs inputs;
+
+    [Header("Settings")]
+    [SerializeField] float moveSpeed = 5.0f;
+
+    [Header("SerializeFields")]
+    Vector2 touchStartPos;
+    bool isTouching = false;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        inputs = new Inputs();
+
+    }
+    void OnEnable()
+    {
+        inputs.Enable();
+    }
+    void OnDisable()
+    {
+        inputs.Disable();
+    }
     void Start()
     {
-        
+        inputs.Touch.Touch.started += context => StartTouch(context);
+        inputs.Touch.Touch.canceled += context => EndTouch(context);
     }
 
-    // Update is called once per frame
+
+    void StartTouch(InputAction.CallbackContext context)
+    {
+        touchStartPos = context.ReadValue<Vector2>();
+        Debug.Log("ASD");
+        Debug.Log(touchStartPos);
+        isTouching = true;
+    }
+
     void Update()
     {
-        
+        if (isTouching)
+        {
+            if (Input.touchCount > 0)
+            {
+                Vector2 touchCurrentPos = Input.GetTouch(0).position;
+                Vector2 touchDelta = touchCurrentPos - touchStartPos;
+                Vector3 moveDirection = new Vector3(touchDelta.x, 0, touchDelta.y);
+                transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+            }
+            else if (Input.GetMouseButton(0)) // Mouse input (left button)
+            {
+                Vector2 mousePos = Input.mousePosition;
+                Vector2 touchDelta = mousePos - touchStartPos;
+                Vector3 moveDirection = new Vector3(touchDelta.x, 0, touchDelta.y);
+                transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    void EndTouch(InputAction.CallbackContext context)
+    {
+        Debug.Log("123");
+        isTouching = false;
     }
 }
